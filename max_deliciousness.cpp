80226fb1edc4cbe433096a139b34aa093beed601
Varad Pawar <122093662+Varadpawar19@users.noreply.github.com>
2023-09-11 23:34:05 +0530

    max_deliciousness.cpp

--- max_deliciousness.cpp
+++ max_deliciousness.cpp
@@ -1,3 +1,15 @@
+// // PROBLEM
+// A new patisserie has opened up to rave reviews. You, in your quest for deliciousness, are going to visit it.
+// The patisserie has N pastries. With your trained eye, you judge that the i-th of them has deliciousness Ai
+// ​Of course, you want to eat pastries whose total deliciousness is as high as possible. Unfortunately, you can't just buy everything out.
+// There are K customers in the store, including you. They form a queue to order pastries, of which you're the L-th person.
+// Each customer, including you, will do the following:
+// Among the remaining pastries, buy the one with the highest deliciousness
+// Then, move to the back of the queue
+// This will repeat till all the pastries are sold out.
+// What's the total deliciousness of the pastries you buy?
+
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
